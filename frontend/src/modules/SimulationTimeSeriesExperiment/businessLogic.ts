e60bbc409dfa302b77b35b89f3d30ceee122b7cf
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2024-01-18 10:17:07 +0100

    Further implementation

--- frontend/src/modules/SimulationTimeSeriesExperiment/businessLogic.ts
+++ frontend/src/modules/SimulationTimeSeriesExperiment/businessLogic.ts
@@ -1,7 +1,34 @@
 export interface BusinessLogicState {
-    ensembleId: string;
+    userSelection: {
+        ensembleIdent: string;
+        vector: string;
+    };
+    utilityData: {
+        vectors: string[];
+    };
 }
 
 export class BusinessLogic {
-    constructor() {}
+    private _state: BusinessLogicState = {
+        userSelection: {
+            ensembleIdent: "",
+            vector: "",
+        },
+        utilityData: {
+            vectors: [],
+        },
+    };
+    private _subscribers: Set<() => void> = new Set();
+
+    subscribe(onStoreChangeCallback: () => void): () => void {
+        this._subscribers.add(onStoreChangeCallback);
+
+        return () => {
+            this._subscribers.delete(onStoreChangeCallback);
+        };
+    }
+
+    getSnapshot(): BusinessLogicState {
+        return this._state;
+    }
 }
--- frontend/src/modules/SimulationTimeSeriesExperiment/settings.tsx
+++ frontend/src/modules/SimulationTimeSeriesExperiment/settings.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 import { ModuleFCProps } from "@framework/Module";
 
+import { BusinessLogicState } from "./businessLogic";
 import { State } from "./state";
 
 //-----------------------------------------------------------------------------------------------------------
 export function Settings({ moduleContext, workbenchSession, workbenchServices }: ModuleFCProps<State>) {
-    return null;
+    const state = React.useSyncExternalStore<BusinessLogicState>();
 }
